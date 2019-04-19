3a42e012ea58d6996b54a02a5c96e25b2036f0bf
Samal14 <49777145+Samal14@users.noreply.github.com>
2019-04-19 12:19:45 +0530

    Add files via upload
    First commit

---
+++ code4.py
@@ -0,0 +1,10 @@
+def lyrics():
+    print("I am a lomberjack and I am Okay")
+    print("I sleep all night and i work all day")
+lyrics()
+def print_twice(unknown):
+    print(unknown)
+    print(unknown)
+print_twice('shakti'*4)
+michael='Eric,the half a bee'
+print_twice(michael)
